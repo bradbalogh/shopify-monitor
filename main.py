88a41028b4873032e19ea792cbaf160e18229200
bbalogh <bbalogh@vailsys.com>
2021-01-01 16:53:27 -0600

    updates

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import requests
 import time
 
 
+def testMonitor(shop_name):
+
+    resp = requests.request(
+        "GET", url=f'https://{shop_name}.com/products.json')
+    data = json.loads(resp.text)
+    products = (data['products'])
+
+    newProductFound(products[0], shop_name)
+
+
+
 def monitor(shop_name, refresh_delay):
 
-    resp = requests.request("GET", url=f'https://{shop_name}.com/products.json')
+    resp = requests.request(
+        "GET", url=f'https://{shop_name}.com/products.json')
     data = json.loads(resp.text)
     products = (data['products'])
 
@@ -14,37 +26,44 @@ def monitor(shop_name, refresh_delay):
     for product in products:
         links.append(product['handle'])
 
-
     # monitor for new products
     while True:
-        resp = requests.request("GET", url=f'https://{shop_name}.com/products.json')
+        resp = requests.request(
+            "GET", url=f'https://{shop_name}.com/products.json')
         data = json.loads(resp.text)
         products = (data['products'])
 
         links = []
-        # initial collection
         for product in products:
             if product['handle'] not in links:
-                newProductFound(product)
+                newProductFound(product, shop_name)
                 links.append(product['handle'])
 
         time.sleep(refresh_delay)
 
 
 
-def newProductFound(product_data):
-    # parse data
+def newProductFound(product_data, shop_name):
+    # parse product data
+    product_title = (product_data['title'])
+    product_link = (f'https://{shop_name}.com/products/'+product_data['handle'])
+    product_image = (product_data['images'][0]['src'])
 
+    # parse variant data
+    variants = product_data['variants']
+    print('$'+variants[0]['price'])
+    for variant in variants:
+        variant_size = variant['title']
+        variant_id = variant['id']
+        print(f'[{variant_size}] {variant_id}')
+
+
+def sendNotification():
     # send discord notification
-
-
-# monitor(shop_name, refresh_delay)
-monitor('prosperskateshop', 4000)
+    print("Sending Notification")
 
 
 
-    # loop through all products
-    # for product in products:
-    #     product_title = (product['title'])
-    #     product_link = (f'https://{shop_name}.com/products/'+product['handle'])
-    #     print(f'{product_title} [{product_link}]')
+#monitor('prosperskateshop', 4000)
+testMonitor('prosperskateshop')
+
