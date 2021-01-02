0dc6aa8f63ed3dca36f2a6d33cbacd8499180e79
bbalogh <bbalogh@vailsys.com>
2021-01-01 18:28:07 -0600

    updates

--- main.py
+++ main.py
@@ -62,13 +62,26 @@ def newProductFound(product_data, shop_name):
     print(sizes)
 
     # /cart/add/id
+    # build size string
+    size_string = ''
+    odd_count = 1
+    for i in range(len(sizes)):
+        size = str(list(sizes.keys())[i])
+        id = str(list(sizes.values())[i])
+        size_string += f'[{size}]' + f'(https://{shop_name}/cart/add/{id})\n'
+        # if odd_count%2 == 0:
+        #     size_string+='\n'
+        # else:
+        #     size_string+='\t'
+        # odd_count+=1
+        
 
     # send discord notification
     webhook = DiscordWebhook(url='https://discord.com/api/webhooks/794705536783613992/RzttvPIL-mDCd00AUh7ArNTRrTCg20EmatzqIAoAFjMY1w1pjorNmapr5AVIng7UvxD2')
     embed = DiscordEmbed(title=product_title, url=product_link, color=0x35e811)
     embed.set_thumbnail(url=product_image)
     embed.add_embed_field(name=price, value='---------', inline=False)
-    embed.add_embed_field(name=price, value='---------', inline=False)
+    embed.add_embed_field(name='Sizes:', value=size_string, inline=False)
     embed.set_footer(text="CyberQuickTask")
     webhook.add_embed(embed)
     webhook.execute()
